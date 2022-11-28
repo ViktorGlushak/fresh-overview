e5803f38471e7c2ec044c9d8a0ae917278bc92d1
Viktor Glushak <v.yu.glushak@tmn2.etagi.com>
2022-11-28 19:19:56 +0500

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# RSS фреймворк Fresh на Deno
+
+
+Используемые технологии:
+1) Preact
+2) Fresh
+3) Deno
+4) Tailwind
+
+Источник: https://www.youtube.com/watch?v=gCkCSyhjLTs
+
+Страница проект:
---
+++ components/Navigation.tsx
@@ -0,0 +1,8 @@
+export default function Navigation() {
+  return (
+    <nav class="px-5 flex h-[50px] absolute top-0 left-0 right-0 bg-blue-400
+    text-white items-center justify-between">
+      <h1 class="text-lg font-bold">Fresh App</h1>
+    </nav>
+  )
+};
